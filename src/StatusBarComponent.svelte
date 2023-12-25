5a61e04ee2c6d705f2da98b872cde3f8c0e7d9dc
Eatgrass <eatgrass@live.cn>
2023-12-25 10:54:45 +0800

    Added a tooltip and context menu for the toolbar timer (#6)
    * feat: add tooltip and context menu for status bar 
    * fix: toggle timer panel
    * feat: polish notification setting

--- src/StatusBarComponent.svelte
+++ src/StatusBarComponent.svelte
@@ -1,16 +1,90 @@
 <script lang="ts">
-import { store, remained } from 'Timer'
+import { store, remained, type Mode } from 'Timer'
 import { settings } from 'stores'
+import { setTooltip, Menu } from 'obsidian'
+
+let statusbar: HTMLElement
+let mode: Mode | undefined
 
 const toggleTimer = () => {
     store.toggleTimer()
 }
+
+$: {
+    if (statusbar && mode !== $store.mode) {
+        mode = $store.mode
+        const tooltip = mode === 'WORK' ? 'Work' : 'Break'
+        setTooltip(statusbar, tooltip, { delay: 300, placement: 'top' })
+    }
+}
+
+const ctxMenu = (e: MouseEvent) => {
+    const menu = new Menu()
+    menu.addItem((item) => {
+        const p = $store.running
+            ? 'Pause'
+            : $store.inSession
+              ? 'Resume'
+              : 'Start'
+        item.setTitle(p).onClick(() => {
+            store.toggleTimer()
+        })
+    })
+
+    menu.addItem((item) => {
+        item.setTitle('Reset').onClick(() => {
+            store.reset()
+        })
+    })
+
+    menu.addItem((item) => {
+        const mode = `Switch ${$store.mode === 'WORK' ? 'Break' : 'Work'} `
+        item.setTitle(mode)
+        item.setDisabled($store.running || $store.inSession)
+        item.onClick(() => {
+            store.toggleMode()
+        })
+    })
+
+    menu.addSeparator()
+
+    menu.addItem((item) => {
+        item.setTitle('Auto-start')
+        item.setChecked($settings.autostart)
+        item.onClick(() => {
+            settings.update((s) => {
+                s.autostart = !s.autostart
+                return s
+            })
+        })
+    })
+
+    menu.addItem((item) => {
+        item.setTitle('Sound')
+        item.setChecked($settings.notificationSound)
+        item.onClick(() => {
+            settings.update((s) => {
+                s.notificationSound = !s.notificationSound
+                return s
+            })
+        })
+    })
+
+    menu.showAtMouseEvent(e)
+}
+
+setTooltip
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 {#if $settings.useStatusBarTimer}
-    <span on:click={toggleTimer} class="st-timer">
+    <span
+        on:click={toggleTimer}
+        class="st-timer"
+        bind:this={statusbar}
+        on:contextmenu={ctxMenu}
+    >
         <span style="margin-right:3px" class="item-icon">
             {#if $store.running}
                 <svg
@@ -28,7 +102,9 @@ const toggleTimer = () => {
                         x1="12"
                         x2="15"
                         y1="14"
-                        y2="11" /><circle cx="12" cy="14" r="8" /></svg>
+                        y2="11"
+                    /><circle cx="12" cy="14" r="8" /></svg
+                >
             {:else}
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -42,9 +118,11 @@ const toggleTimer = () => {
                     stroke-linejoin="round"
                     class="lucide lucide-timer-off"
                     ><path d="M10 2h4" /><path
-                        d="M4.6 11a8 8 0 0 0 1.7 8.7 8 8 0 0 0 8.7 1.7" /><path
-                        d="M7.4 7.4a8 8 0 0 1 10.3 1 8 8 0 0 1 .9 10.2" /><path
-                        d="m2 2 20 20" /><path d="M12 12v-2" /></svg>
+                        d="M4.6 11a8 8 0 0 0 1.7 8.7 8 8 0 0 0 8.7 1.7"
+                    /><path
+                        d="M7.4 7.4a8 8 0 0 1 10.3 1 8 8 0 0 1 .9 10.2"
+                    /><path d="m2 2 20 20" /><path d="M12 12v-2" /></svg
+                >
             {/if}
         </span>
         {$remained.human}
@@ -55,7 +133,7 @@ const toggleTimer = () => {
 .item-icon {
     padding-top: 0;
 }
-.st-timer{
+.st-timer {
     display: inline-flex;
     align-items: center;
 }
