96426d13019a59b95e33d77b78d93611defa5d53
Huang Jing <eatgrass@live.cn>
2024-01-10 11:01:21 +0800

    feat: render markdown task item

---
+++ src/TaskItemComponent.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { afterUpdate } from 'svelte'
+
+export let render: (content: string, el: HTMLElement) => void
+export let content: string
+let el: HTMLDivElement
+afterUpdate(() => {
+    el.empty()
+    render(content, el)
+})
+</script>
+
+<div bind:this={el} class="pomodoro-tasks-item-desc"></div>
+
+<style>
+.pomodoro-tasks-item-desc {
+    width: 100%;
+    pointer-events: none;
+    cursor: pointer;
+}
+</style>
